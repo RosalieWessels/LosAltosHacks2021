42265742c898e101fdd546f30f8f352bb3e9c511
RosalieWessels <rosalie.wessels@yahoo.com>
2021-04-24 08:03:43 -0700

    Initial Commit

---
+++ Password Manager/Password_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_ManagerApp.swift
+//  Password Manager
+//
+//  Created by Rosalie Wessels on 4/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Password_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
